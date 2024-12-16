dce3ce56306274277c8a773713c7a6b8886246c8
amalamud <astridmalamud@gmail.com>
2024-12-16 12:31:21 -0300

    SCRUM-19 BONUS-US create back office controller, service, and create structure to work on

---
+++ src/main/java/com/mercadolibre/be_java_hisp_w28_g10/service/impl/BackOfficeServiceImpl.java
@@ -0,0 +1,42 @@
+package com.mercadolibre.be_java_hisp_w28_g10.service.impl;
+
+import com.mercadolibre.be_java_hisp_w28_g10.enums.ReportTypeEnum;
+import com.mercadolibre.be_java_hisp_w28_g10.exception.BadRequestException;
+import com.mercadolibre.be_java_hisp_w28_g10.service.IBackOfficeService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BackOfficeServiceImpl implements IBackOfficeService {
+
+    @Override
+    public String getReport(String reportName, String order, int top) {
+        String csvResult = "";
+        try{
+            ReportTypeEnum reportType = ReportTypeEnum.valueOf(reportName);
+            switch (reportType){
+                case USERS_BY_FOLLOWERS:
+
+                    break;
+                case USERS_BY_FOLLOWS:
+
+                    break;
+                case USERS_BY_POSTS:
+
+                    break;
+                case POSTS_BY_PRICE:
+
+                    break;
+                case POSTS_BY_DISCOUNT:
+
+                    break;
+                case POSTS_BY_DATE:
+
+                    break;
+            }
+        }catch (IllegalArgumentException e){
+            throw new BadRequestException("Invalid report name");
+        }
+
+        return csvResult;
+    }
+}
