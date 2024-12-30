2b212cc4530966298a152804f9e1a636268671cb
wnicolas <nicolas.buitrago2@gmail.com>
2024-12-30 10:43:44 -0500

    feature/SCRUM47 Adding valid annotations for product, user and post dto's

--- src/main/java/com/mercadolibre/be_java_hisp_w28_g10/dto/post/PostDTO.java
+++ src/main/java/com/mercadolibre/be_java_hisp_w28_g10/dto/post/PostDTO.java
@@ -1,6 +1,8 @@
 package com.mercadolibre.be_java_hisp_w28_g10.dto.post;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,15 +12,30 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PostDTO {
+    @NotNull(message = "El id no puede estar vacío.")
+    @Positive(message = "El id debe ser mayor a cero.")
     @JsonProperty("user_id")
     private int id;
+
     @JsonProperty("post_id")
     private int postId;
+
+    @NotNull(message = "La fecha no puede estar vacía.")
     private String date;
+
+    @NotNull(message = "El campo no puede estar vacío.")
     private int category;
+
+    @NotNull(message = "El campo no puede estar vacío.")
+    @DecimalMin(value = "0.0", inclusive = false, message = "El precio no puede ser 0.")
+    @DecimalMax(value = "10000000.0", message = "El precio máximo por producto es de 10.000.000.")
     private double price;
+
+    @Valid
     private ProductDTO product;
+
     @JsonProperty("has_promo")
     private boolean hasPromo;
+
     private double discount;
 }
--- src/main/java/com/mercadolibre/be_java_hisp_w28_g10/dto/user/UserDTO.java
+++ src/main/java/com/mercadolibre/be_java_hisp_w28_g10/dto/user/UserDTO.java
@@ -1,5 +1,6 @@
 package com.mercadolibre.be_java_hisp_w28_g10.dto.user;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDTO {
+    @NotNull(message = "El id no puede estar vacío.")
+    @Positive(message = "El id debe ser mayor a cero.")
     private int id;
     private String name;
 }
