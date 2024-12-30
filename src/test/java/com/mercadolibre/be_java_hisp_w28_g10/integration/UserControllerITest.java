ec54db0db0a16f5f82cd3c0cce2b87fb9c20b341
santiagoparrado <santi.542001@gmail.com>
2024-12-30 16:02:42 -0500

    feature/SCRUM-51 add unit test and parcial integration test

--- src/test/java/com/mercadolibre/be_java_hisp_w28_g10/integration/UserControllerITest.java
+++ src/test/java/com/mercadolibre/be_java_hisp_w28_g10/integration/UserControllerITest.java
@@ -1,22 +1,44 @@
 package com.mercadolibre.be_java_hisp_w28_g10.integration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mercadolibre.be_java_hisp_w28_g10.dto.follow.FollowersDTO;
+import com.mercadolibre.be_java_hisp_w28_g10.model.FollowRelation;
+import com.mercadolibre.be_java_hisp_w28_g10.model.User;
+import com.mercadolibre.be_java_hisp_w28_g10.repository.impl.UserRepositoryImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+
+import java.util.Set;
+
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class UserControllerITest {
 
+    private UserRepositoryImpl userRepository;
+    private User user;
     @Autowired
     private MockMvc mockMvc;
-
+    @Autowired
+    private ObjectMapper objectMapper;
     @BeforeEach
     void setUp() {
+        user = new User(1, "Test User");
+        userRepository.getFollowRelationsByFollowerId(user.getId()); // Guarda el usuario en la base de datos
     }
+    private static final FollowRelation followRelation1 = new FollowRelation(10, 1);
+    private static final FollowRelation followRelation2 = new FollowRelation(11, 1);
 
     @Test
     void getAllUsers() {
@@ -31,7 +53,21 @@ class UserControllerITest {
     }
 
     @Test
-    void getAmountFollowersById() {
+    void getAmountFollowersById() throws Exception {
+        //ARRANGE
+        int userId = 1;
+        FollowRelation followEsperado = (FollowRelation) Set.of(followRelation1, followRelation2);
+        ResultMatcher statusEsperado = status().isOk();
+        ResultMatcher contentTypeEsperado = (ResultMatcher) content().contentType("application/json");
+        ResultMatcher bodyEsperado = (ResultMatcher) content().json(objectMapper.writeValueAsString(followEsperado));
+
+        // ACT AND ASSERT
+        mockMvc.perform(get("users/{userId}/followers/count", userId))
+                .andExpect(status().isOk())
+                .andExpect(statusEsperado)
+                .andExpect(contentTypeEsperado)
+                .andExpect(bodyEsperado)
+                .andDo(print());
     }
 
     @Test
--- src/test/java/com/mercadolibre/be_java_hisp_w28_g10/unit/impl/UserServiceimplTest.java
+++ src/test/java/com/mercadolibre/be_java_hisp_w28_g10/unit/impl/UserServiceimplTest.java
@@ -1,9 +1,11 @@
 package com.mercadolibre.be_java_hisp_w28_g10.unit.impl;
 
-import com.mercadolibre.be_java_hisp_w28_g10.model.Product;
+import com.mercadolibre.be_java_hisp_w28_g10.dto.follow.FollowersDTO;
+import com.mercadolibre.be_java_hisp_w28_g10.exception.NotFoundException;
+import com.mercadolibre.be_java_hisp_w28_g10.model.FollowRelation;
+import com.mercadolibre.be_java_hisp_w28_g10.model.User;
 import com.mercadolibre.be_java_hisp_w28_g10.repository.IProductRepository;
 import com.mercadolibre.be_java_hisp_w28_g10.repository.IUserRepository;
-import com.mercadolibre.be_java_hisp_w28_g10.service.impl.ProductServiceImpl;
 import com.mercadolibre.be_java_hisp_w28_g10.service.impl.UserServiceimpl;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,7 +13,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -28,6 +29,8 @@ class UserServiceimplTest {
 
     @InjectMocks
     private UserServiceimpl userService;
+    private User user;
+    private List<FollowRelation> followRelations;
 
     @Test
     void getAllUsers() {
@@ -44,13 +47,33 @@ class UserServiceimplTest {
     @Test
     void follow() {
     }
-
     @Test
-    void getFollowersAmountById() {
+    void getUserFollowersAmountById_followersComplete_happyPath() {
+        user = new User(2, "Pedro");
+        //ARRANGE
+        FollowRelation followRelation1 = new FollowRelation(10, user.getId());
+        FollowRelation followRelation2 = new FollowRelation(1, user.getId());
+        FollowRelation followRelation3 = new FollowRelation(11, user.getId());
+        followRelations = Arrays.asList(followRelation1, followRelation2, followRelation3);
+        when(userRepository.findUserById(1)).thenReturn(user);
+        when(userRepository.findAllFollowRelation()).thenReturn(followRelations);
+        //ACT
+        FollowersDTO result = userService.getFollowersAmountById(1);
+        //ASSERT
+        assertNotNull(result);
+        assertEquals(user.getId(), result.getId());
+        assertEquals(user.getName(), result.getName());
+        assertEquals(3, result.getFollowersCount());
     }
-
     @Test
-    void getUserFollowersById() {
+    void testGetFollowersAmountById_UserNotFound_badPath() {
+        // ARRANGE
+        when(userRepository.findUserById(1)).thenReturn(null);
+
+        // ACT
+        NotFoundException exception = assertThrows(NotFoundException.class, () -> userService.getFollowersAmountById(1));
+        //ASSERT
+        assertEquals("User not found", exception.getMessage());
     }
 
     @Test
