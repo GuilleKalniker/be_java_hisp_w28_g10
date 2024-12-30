3c3378ed4b286d92f923a16de36a49b76449c6ef
wnicolas <nicolas.buitrago2@gmail.com>
2024-12-30 14:38:45 -0500

    feature/SCRUM47 Adding mocks annotations for integration a unit tes

--- src/test/java/com/mercadolibre/be_java_hisp_w28_g10/integration/ProductControllerITest.java
+++ src/test/java/com/mercadolibre/be_java_hisp_w28_g10/integration/ProductControllerITest.java
@@ -2,8 +2,18 @@ package com.mercadolibre.be_java_hisp_w28_g10.integration;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 class ProductControllerITest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
     @BeforeEach
     void setUp() {
 
--- src/test/java/com/mercadolibre/be_java_hisp_w28_g10/unit/impl/UserServiceimplTest.java
+++ src/test/java/com/mercadolibre/be_java_hisp_w28_g10/unit/impl/UserServiceimplTest.java
@@ -1,10 +1,32 @@
 package com.mercadolibre.be_java_hisp_w28_g10.unit.impl;
 
+import com.mercadolibre.be_java_hisp_w28_g10.model.Product;
+import com.mercadolibre.be_java_hisp_w28_g10.repository.IProductRepository;
+import com.mercadolibre.be_java_hisp_w28_g10.repository.IUserRepository;
+import com.mercadolibre.be_java_hisp_w28_g10.service.impl.ProductServiceImpl;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class UserServiceimplTest {
+    @Mock
+    private IProductRepository productRepository;
+
+    @Mock
+    private IUserRepository userRepository;
+
+    @InjectMocks
+    private ProductServiceImpl productService;
 
     @Test
     void getAllUsers() {
