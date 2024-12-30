1e7156d84a8592ab44f1c59c1dfcf0b79e63f009
wnicolas <nicolas.buitrago2@gmail.com>
2024-12-30 10:01:48 -0500

    Adding scafolding classes for unit and integrated test

---
+++ src/test/java/com/mercadolibre/be_java_hisp_w28_g10/integration/UserControllerITest.java
@@ -0,0 +1,39 @@
+package com.mercadolibre.be_java_hisp_w28_g10.integration;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class UserControllerITest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAllUsers() {
+    }
+
+    @Test
+    void getAllFollowRelations() {
+    }
+
+    @Test
+    void addNewFollow() {
+    }
+
+    @Test
+    void getAmountFollowersById() {
+    }
+
+    @Test
+    void unfollowUserById() {
+    }
+
+    @Test
+    void getUserFollowersById() {
+    }
+
+    @Test
+    void getUserFollowedById() {
+    }
+}
