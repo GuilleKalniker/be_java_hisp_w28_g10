4048b2ce071c719277a913df60d79edee5e8ab05
santiagoparrado <santi.542001@gmail.com>
2025-01-02 10:36:28 -0500

    feature/SCRUM-57 Add integration test and fix unit test code

--- src/test/java/com/mercadolibre/be_java_hisp_w28_g10/integration/UserControllerITest.java
+++ src/test/java/com/mercadolibre/be_java_hisp_w28_g10/integration/UserControllerITest.java
@@ -7,6 +7,8 @@ import com.mercadolibre.be_java_hisp_w28_g10.model.User;
 import com.mercadolibre.be_java_hisp_w28_g10.repository.impl.UserRepositoryImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,18 +16,19 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
 
 
+import java.util.List;
 import java.util.Set;
 
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class UserControllerITest {
-
+    @Mock
     private UserRepositoryImpl userRepository;
     private User user;
     @Autowired
@@ -34,11 +37,11 @@ class UserControllerITest {
     private ObjectMapper objectMapper;
     @BeforeEach
     void setUp() {
-        user = new User(1, "Test User");
-        userRepository.getFollowRelationsByFollowerId(user.getId()); // Guarda el usuario en la base de datos
+        MockitoAnnotations.openMocks(this);
+        user = new User(1, "Alice");
+        userRepository.getFollowRelationsByFollowerId(user.getId());
     }
-    private static final FollowRelation followRelation1 = new FollowRelation(10, 1);
-    private static final FollowRelation followRelation2 = new FollowRelation(11, 1);
+    private static final FollowRelation followRelation1 = new FollowRelation(1, 10);
 
     @Test
     void getAllUsers() {
@@ -51,24 +54,22 @@ class UserControllerITest {
     @Test
     void addNewFollow() {
     }
-
     @Test
-    void getAmountFollowersById() throws Exception {
-        /*ARRANGE
+    void getAmountFollowersById_happyPath() throws Exception {
+        // ARRANGE
         int userId = 1;
-        FollowRelation followEsperado = (FollowRelation) Set.of(followRelation1, followRelation2);
-        ResultMatcher statusEsperado = status().isOk();
-        ResultMatcher contentTypeEsperado = (ResultMatcher) content().contentType("application/json");
-        ResultMatcher bodyEsperado = (ResultMatcher) content().json(objectMapper.writeValueAsString(followEsperado));
+        List<FollowRelation> followEsperado = List.of(followRelation1);
+        when(userRepository.getFollowRelationsByFollowerId(userId)).thenReturn(followEsperado);
+
+        FollowersDTO expectedResponse = new FollowersDTO(userId, user.getName(), followEsperado.size());
+        String expectedJson = objectMapper.writeValueAsString(expectedResponse);
 
         // ACT AND ASSERT
-        mockMvc.perform(get("users/{userId}/followers/count", userId))
+        mockMvc.perform(get("/users/{userId}/followers/count", userId))
                 .andExpect(status().isOk())
-                .andExpect(statusEsperado)
-                .andExpect(contentTypeEsperado)
-                .andExpect(bodyEsperado)
+                .andExpect(content().contentType("application/json"))
+                .andExpect(content().json(expectedJson))
                 .andDo(print());
-        */
     }
 
     @Test
